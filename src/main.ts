6d6d38401668ed1d93cba8fd52da15024ac2e68e
sohhyeonkim <sohhyeonkim@gmail.com>
2024-04-07 14:17:34 +0900

    [chore]: install packages - config - sentry

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,18 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import * as Sentry from "@sentry/node";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   await app.listen(3000);
+
+  Sentry.init({
+    dsn: process.env.SENTRY_DSN,
+
+    // We recommend adjusting this value in production, or using tracesSampler
+    // for finer control
+    tracesSampleRate: 1.0,
+  });
+
 }
 bootstrap();
